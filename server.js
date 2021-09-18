d64e6a981a53033e4bb470300661690d3d525262
Dawn Wi <wi825@naver.com>
2021-09-18 14:31:35 +0900

    get초안

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080,function(){
+    console.log('listening on 8080')
+});
+
+app.get('/pet',function(요청, 응답){
+    응답.send('펫용품 페이지');
+}); 
+
+app.get('/beauty',function(요청, 응답){
+    응답.send('뷰티용품 페이지');
+}); 
+
+app.get('/',function(요청, 응답){
+    응답.sendFile(__dirname+'/index.html');
+}); 
